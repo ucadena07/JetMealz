ac459b03cbc18b00376634642b262a4e86f4665c
Ulises Cadena <ucadena07@gmail.com>
2024-02-27 06:50:58 -0600

    customizing the meal screen

--- app/src/main/java/com/example/mealzapp/screens/mealCategories/MealCategoriesScreen.kt
+++ app/src/main/java/com/example/mealzapp/screens/mealCategories/MealCategoriesScreen.kt
@@ -1,25 +1,75 @@
 package com.example.mealzapp.screens.MealCategories
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.example.mealzapp.model.Category
 import com.example.mealzapp.screens.mealCategories.MealCategoriesViewModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+
 
 @Composable
 fun MealsCategoryScreen(vm: MealCategoriesViewModel) {
 
-    LazyColumn{
-        items(vm.meals.value){
-            Text(text = it.strCategory)
+    Surface(modifier = Modifier.fillMaxSize().padding(12.dp)) {
+        LazyColumn{
+            items(vm.meals.value){
+                MealCategory(meal = it)
+            }
         }
     }
+
+}
+
+
+@Composable
+fun MealCategory(meal: Category){
+    Card(shape = RoundedCornerShape(8.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(4.dp)) {
+        Row {
+            AsyncImage(
+                model = meal.strCategoryThumb,
+                contentDescription = "image",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(88.dp)
+                    .padding(4.dp),
+            )
+            Column(modifier = Modifier
+                .align(Alignment.CenterVertically)
+                .padding(16.dp)) {
+                Text(text = meal.strCategory, style = MaterialTheme.typography.headlineMedium)
+                Text(text = meal.strCategoryDescription, style = MaterialTheme.typography.titleMedium)
+            }
+        }
+    }
+
+}
+
+@Composable
+@Preview(showBackground = true)
+fun MealsCategoryScreenPreview(){
+
+        MealCategory(meal = Category(idCategory = "1","Lamb","Lamb, hogget, and mutton are the meat of domestic sheep (species Ovis aries) at different ages",""))
+
 }
